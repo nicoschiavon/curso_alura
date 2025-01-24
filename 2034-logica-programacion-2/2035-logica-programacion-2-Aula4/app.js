2fc3ecbe847984048c9422244c722dc84c8e9b98
nicoschiavon <nico.schiavon@gmail.com>
2025-01-24 20:30:32 -0300

    Actualizo el app.js y el index.html

--- 2034-logica-programacion-2/2035-logica-programacion-2-Aula4/app.js
+++ 2034-logica-programacion-2/2035-logica-programacion-2-Aula4/app.js
@@ -1,7 +1,8 @@
 let numeroSecreto = 0;
-let intentos = 0;
+let intentos = 1;
 let listaNumerosSorteados = [];
 let numeroMaximo = 10;
+let intentosMaximos = 0;
 
 
 function asignarTextoElemento(elemento, texto) {
@@ -13,15 +14,24 @@ function asignarTextoElemento(elemento, texto) {
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     console.log(intentos)
-    if (numeroDeUsuario === numeroSecreto) {
+    if ((numeroDeUsuario === numeroSecreto) && (intentos <= intentosMaximos)) {
         asignarTextoElemento('#info__juego',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
         document.getElementById('reiniciar').removeAttribute('disabled');
+        document.querySelector('#jugar').setAttribute('disabled','true');
+
     } else {
         //El usuario no acertó.
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('#info__juego','El número secreto es menor');
+        if (intentosMaximos - intentos == 0) {
+            asignarTextoElemento('#info__juego',`Se te acabaron los intentos!, ¿Deseas volver a jugar?`);
+            document.getElementById('reiniciar').removeAttribute('disabled');
+            document.querySelector('#jugar').setAttribute('disabled','true');
+
         } else {
-            asignarTextoElemento('#info__juego','El número secreto es mayor');
+            if (numeroDeUsuario > numeroSecreto) {
+                asignarTextoElemento('#info__juego',`El número secreto es menor, Tienes ${intentosMaximos - intentos} disponibles`);
+            } else {
+                asignarTextoElemento('#info__juego',`El número secreto es mayor, Tienes ${intentosMaximos - intentos} disponibles`);
+            }
         }
         intentos++;
         limpiarCaja();
@@ -58,36 +68,39 @@ function condicionesIniciales() {
 
     asignarTextoElemento('h1','Juego del número secreto!');
     asignarTextoElemento('#p__seleccion', 'Elija la dificultad del juego');
-    asignarTextoElemento('#intentos', 'Elija la cantidad de intentos');
-    asignarTextoElemento('#intentos__extra', 'O ingrese la cantidad que usted desea');
+    asignarTextoElemento('#intentos', 'Elija la cantidad de intentos máximos');
     // asignarTextoElemento('#info__juego',`Indica un número del 1 al ${numeroMaximo}`);
     // numeroSecreto = generarNumeroSecreto();
-    // intentos = 1;
+    intentos = 1;
     // console.log(numeroSecreto);
 }
 
 function reiniciarJuego() {
     //limpiar caja
-    limpiarCaja();
     //Indicar mensaje de intervalo de números 
     //Generar el número aleatorio
     //Inicializar el número intentos
+    document.getElementById('menu__seleccion__juego').removeAttribute('hidden');
+    document.getElementById('menu__juego').setAttribute('hidden','true');
+    limpiarCaja();
+    asignarTextoElemento('#info__juego',"");
+
     condicionesIniciales();
     //Deshabilitar el botón de nuevo juego
     document.querySelector('#reiniciar').setAttribute('disabled','true');
+    document.getElementById('jugar').removeAttribute('disabled');
+
     
 }
 
 // Agregar cantidad de intentos
 
 function agregarIntentos() {
-    intentos = document.getElementById('valorIntentos').value;
-    if (intentos == "") {
-        console.log('si pasa');
-        intentos = document.getElementById('intentos__select').value;
-    } 
-    console.log(intentos);
-    intentos = parseInt(intentos);
+    intentosMaximos = document.getElementById('intentos__select').value;
+    console.log('si pasa');
+    
+    console.log(intentosMaximos);
+    intentosMaximos = parseInt(intentosMaximos);
     document.getElementById('menu__seleccion__juego').setAttribute('hidden','true');
     document.getElementById('menu__juego').removeAttribute('hidden');
     asignarTextoElemento('#p__seleccion',`Indica un número del 1 al ${numeroMaximo}`);
